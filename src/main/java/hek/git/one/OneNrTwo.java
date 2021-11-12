64972cd742cbc60dff9c3a3dd5790838a12c960d
Heiko Kolter <heiko.kolter@gmx.de>
2021-11-12 08:39:45 +0100

    merge changes from one
    one 8 changed on former rebased class
    one 7  some change
    one 7 merged OneNrTwo.java with master

--- src/main/java/hek/git/one/OneNrTwo.java
+++ src/main/java/hek/git/one/OneNrTwo.java
@@ -1,13 +1,32 @@
 package hek.git.one;
 
-public class OneNrTwo {
-    //change in on
+import java.util.Objects;
 
-    public OneNrTwo() {
+public class OneNrTwo {
+    private final String name;
+
+
+    public OneNrTwo(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OneNrTwo oneNrTwo = (OneNrTwo) o;
+        return Objects.equals(name, oneNrTwo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     @Override
     public String toString() {
-        return "OneNrTwo{}";
+        return "OneNrTwo{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
