497c38c34b794b7dd2923fb2fed4fc6948334257
Heiko Kolter <heiko.kolter@gmx.de>
2021-11-12 08:37:45 +0100

    master 9 changed OneNrTwo.java parallel

--- src/main/java/hek/git/one/OneNrTwo.java
+++ src/main/java/hek/git/one/OneNrTwo.java
@@ -2,4 +2,12 @@ package hek.git.one;
 
 public class OneNrTwo {
     //change in on
+
+    public OneNrTwo() {
+    }
+
+    @Override
+    public String toString() {
+        return "OneNrTwo{}";
+    }
 }
